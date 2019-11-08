41e2d9596e1928e44c38510fc1dc343d9123852d
beserge <bensergentanis@gmail.com>
2019-11-07 20:25:22 -0500

    Update rev2.ino
    added inputs for all parameters. TODO, squash any add'l feedback/ clicking problems I deem worthy of my attention

--- teensy code/rev2.ino
+++ teensy code/rev2.ino
@@ -1,3 +1,5 @@
+#include <Bounce.h>
+
 #include <ADC.h>
 
 #include <Audio.h>
@@ -15,59 +17,58 @@ AudioConnection          patchCord2(outputQueue, 0, pt8211_1, 0);
 AudioConnection          patchCord3(outputQueue, 0, pt8211_1, 1);
 // GUItool: end automatically generated code*/
 
-float feedback = 0.8;
+float feedback = .8;
 short int input = 0;
-const short int delayMax = 3000;
+const short int delayMax = 15000;
 short int sampleBuffer [delayMax];
 short int delayBuffer[delayMax];
 short int delayNum = 0;
-short int delayRate = 1;
+short int delayRate = 0;
 bool sampleBool = false;
 short int sampleNum = 0;
 short int sampleLen = delayMax;
 short int outputBuffer[256];
+float sub = 0;
 
-IntervalTimer sampleOut;
+
+int samplerate = 0;
+short int sampleButton = 17;
+short int resetButton = 15;
+int modCount = 0;
+
+IntervalTimer outputTimer;
+IntervalTimer inputTimer;
 int counter = 0;
 
 void setup() {
-  uint32_t freq = 22058;  // try a sampling rate of 22.058 kHz (44.117 kHz is base, so we'll do half)
+  //uint32_t freq = 22058;  // try a sampling rate of 22.058 kHz (44.117 kHz is base, so we'll do half)
+  //pin 7,20,21 used by DAC
+  //aka A6, A7
+  pinMode(sampleButton, INPUT);
+  pinMode(resetButton, INPUT);
 
-  adc->adc0->stopPDB();
-  adc->adc0->startSingleRead(A1);
-  adc->enableInterrupts(ADC_0);
-  adc->adc0->startPDB(freq); //frequency in Hz
+  outputTimer.begin(outInterrupt, 45.334);
+  inputTimer.begin(inInterrupt, 400);
 
-  adc->adc1->stopPDB();
-  adc->adc1->startSingleRead(A9);
-  adc->enableInterrupts(ADC_1);
-  adc->adc1->startPDB(freq); //frequency in Hz
+  adc->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED);
+  adc->setSamplingSpeed(ADC_SAMPLING_SPEED:: HIGH_SPEED);
+  adc->setAveraging(32);
+  adc->setResolution(12);
 
-
-  AudioMemory(20);
+  AudioMemory(30);
 
   for (short int i = 0; i < delayMax; i++) {
     delayBuffer[i] = 0;
     sampleBuffer[i] = 0;
   }
 
-  adc->setConversionSpeed(ADC_CONVERSION_SPEED:: HIGH_SPEED);
-  adc->setSamplingSpeed(ADC_SAMPLING_SPEED:: HIGH_SPEED);
-  adc->setAveraging(32);
-  adc->setResolution(12);
-
-  adc->setAveraging(16, ADC_1); // set number of averages
-  adc->setResolution(12, ADC_1); // set bits of resolution
-  adc->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED, ADC_1); // change the conversion speed
-  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED, ADC_1); // change the sampling speed
-
 }
 
 
-void adc0_isr() {
+void outInterrupt() {
   cli();
   //adc->adc0->readSingle();
-  input = (adc->readSingle(A1)) * 2;
+  input = (adc->analogRead(A2)) * 2;
   if (counter == 128) {
     getNextOutputBlock();
     counter = 0;
@@ -78,25 +79,41 @@ void adc0_isr() {
   sei();
 }
 
-void adc1_isr() {
-  //cli();
-  adc->adc1->readSingle(); 
-  feedback = (float)(adc->readSingle(A9)) / (float)8000;
-  //sei();
+void inInterrupt() {
+  //Note: pins A7 and A6 are used by DAC
+  //aka 20 and 21
+  feedback = (float)(adc->analogRead(A4)) / 4500;
+  delayRate = map(adc->analogRead (A5), 0, 8535, 1, 30);
+  samplerate = map(adc->analogRead (A9), 0, 8535, 0, 10);
+  sampleBool = digitalRead(sampleButton);
+  sub = .7 * digitalRead(resetButton);
 }
 
 void getNextOutputBlock() {
 
   //quickly update sample and delay buffers, then update output buffer and pass it to DAC
   for (int i = 0; i < 128; i += 2) {
+    checkLen();
+    if (sampleNum >= delayMax)
+      sampleNum = sampleNum - delayMax;
     short int holdNum = outputBuffer[i];
-    outputBuffer[i] = outputBuffer[i] + delayBuffer[delayNum];
+    outputBuffer[i] = (outputBuffer[i] + delayBuffer[delayNum]) * 2 + sampleBuffer[sampleNum];
     //outputBuffer[i] = outputBuffer[i];
     outputBuffer[i + 1] = outputBuffer[i];
 
     updateDelay(holdNum);
+    if (modCount >= samplerate) {
+      if (sampleBool) {
+        updateSample();
+      }
+      else {
+        sampleNum++;
+        modCount = 0;
+      }
+    }
+    modCount++;
+    sampleBuffer[sampleNum] -= sampleBuffer[sampleNum] * sub;
   }
-
   int16_t *p = outputQueue.getBuffer();
   memcpy(p, outputBuffer, 256);
   outputQueue.playBuffer();
@@ -117,20 +134,31 @@ void updateDelay(short int in) {
   for (short int i = 0; i < delayRate; i++) {
     if (delayNum >= delayMax)
       delayNum = delayNum - delayMax;
-    delayBuffer[delayNum] = ((float)feedback * (float)delayBuffer[delayNum] + in);
+
+    int item = ((float)feedback * (float)delayBuffer[delayNum] + in);
+    if (item > 7000 || item < delayBuffer[delayNum] || item < delayBuffer[delayNum]) {
+      item = .6 * in;
+    }
+    delayBuffer[delayNum] = feedback * delayBuffer[delayNum] + in;
     delayNum++;
   }
 }
 
 void updateSample() {
-  for (short int i = 0; i < delayMax; i++) {
-    if (sampleNum == delayMax)
-      sampleNum = 0;
-    sampleBuffer[sampleNum] = delayBuffer[delayNum] + sampleBuffer[sampleNum];
-    sampleNum++;
+  modCount = 1;
+  sampleNum++;
+  int item = .8 * sampleBuffer[sampleNum] + delayBuffer[delayNum];
+  if (item > 8000 || item < sampleBuffer[sampleNum] || item < sampleBuffer[delayNum]) {
+    item = .1 * sampleBuffer[sampleNum] + .6 * delayBuffer[delayNum];
+  }
+  sampleBuffer[sampleNum] = item;
+}
+
+void resetBuffer() {
+  for (int i = 0; i < delayMax; i++) {
+    sampleBuffer[i] = 0;
   }
 }
 
 void loop() {
-
 }
